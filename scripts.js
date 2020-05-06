e54f9976cd9805c4cdb9589949a364975c6784a2
lacsi8028 <64015978+lacsi8028@users.noreply.github.com>
2020-05-06 16:54:50 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,47 @@
-let data = {
-    photo: 'Images/dariusz-sankowski-3OiYMgDKJ6k-unsplash.jpg',
-    title: 'This is a begining of a wonderfull time',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed justo dolor. Praesent convallis est ullamcorper risus tincidunt, eget viverra ante convallis. Duis rhoncus blandit accumsan.'
-  };
+let currentPhoto = 0;
 
-let currentPhoto = 2;
+let imagesData = [{
+    photo: "Images/dariusz-sankowski-3OiYMgDKJ6k-unsplash.jpg",
+    title: "This is a begining of a wonderfull time",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed justo dolor. Praesent convallis est ullamcorper risus tincidunt, eget viverra ante convallis. Duis rhoncus blandit accumsan.",
+  },
+  {
+    photo: "Images/karim-manjra-QZv2PEXUk7c-unsplash.jpg",
+    title: "Back to the starts",
+    description: "Ut pretium molestie purus, vitae ornare tellus accumsan vel. Donec congue mi vitae rhoncus placerat. Nunc id tempus enim, at cursus tortor.",
+  },
+  {
+    photo: "Images/daniel-vogel-sVothhm7iRI-unsplash.jpg",
+    title: "The time machine",
+    description: "Fusce ipsum sem, fermentum sed quam eget, vestibulum fermentum lacus. In posuere tellus sit amet volutpat tempor. Sed feugiat lectus orci, quis mattis augue luctus eget.",
+  },
+  {
+    photo: "Images/nasa-U2uKrI4lci8-unsplash.jpg",
+    title: "Small step ",
+    description: "Donec pretium nunc ex, in iaculis ex scelerisque in. Pellentesque a tellus vel arcu cursus commodo a iaculis arcu.",
+  },
+  {
+    photo: "Images/roman-kraft-g_gwdpsCVAY-unsplash.jpg",
+    title: "Buildings in a cosy town",
+    description: "Nunc vel convallis ipsum. Mauris luctus turpis orci, in consectetur nisi aliquet a. Duis vitae rhoncus eros.",
+  },
+  {
+    photo:  "Images/subtle-cinematics-VHt_sWcj70I-unsplash.jpg",
+    title: "Under pressure",
+    description: "Praesent enim sapien, consequat sed congue vitae, semper et arcu. Sed vel ultrices ex, eu fermentum mi.",
+  },
+  {
+    photo: "Images/troy-taylor-8Qvbsy3B7dE-unsplash.jpg",
+    title: "Northern lights",
+    description: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.",
+  }
+];
+
 
 let loadPhoto = (photoNumber) => {
-    $('#photo').attr('src', data.photo);
-$('#photo-title').text(data.title);
-$('#photo-description').text(data.description);
+  $('#photo').attr('src', imagesData[currentPhoto].photo);
+  $('#photo-title').text(imagesData[currentPhoto].title);
+  $('#photo-description').text(imagesData[currentPhoto].description);
 }
 
 $('#previus').click(() => {
@@ -28,7 +60,7 @@ $('#next').click(() => {
 
 loadPhoto(currentPhoto);
 
-var images = [
+var photo = [
     "Images/dariusz-sankowski-3OiYMgDKJ6k-unsplash.jpg",
     "Images/karim-manjra-QZv2PEXUk7c-unsplash.jpg",
     "Images/daniel-vogel-sVothhm7iRI-unsplash.jpg",
@@ -38,30 +70,22 @@ var images = [
     "Images/troy-taylor-8Qvbsy3B7dE-unsplash.jpg"
 ];
 
-var subtitles = [
-    "This is a begining of a wonderfull time",
-    "Back to the starts",
-    "The time machine",
-    "Small steps ",
-    "Buildings in a cosy town",
-    "under pressure",
-    "Northern lights"
+var titles = [
+  "This is a begining of a wonderfull time",
+  "Back to the starts",
+  "The time machine",
+  "Small steps ",
+  "Buildings in a cosy town",
+  "under pressure",
+  "Northern lights"
 ];
 
-var explanations = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed justo dolor. Praesent convallis est ullamcorper risus tincidunt, eget viverra ante convallis. Duis rhoncus blandit accumsan.",
-    "Ut pretium molestie purus, vitae ornare tellus accumsan vel. Donec congue mi vitae rhoncus placerat. Nunc id tempus enim, at cursus tortor.",
-    "Fusce ipsum sem, fermentum sed quam eget, vestibulum fermentum lacus. In posuere tellus sit amet volutpat tempor. Sed feugiat lectus orci, quis mattis augue luctus eget.",
-    "Donec pretium nunc ex, in iaculis ex scelerisque in. Pellentesque a tellus vel arcu cursus commodo a iaculis arcu.",
-    "Nunc vel convallis ipsum. Mauris luctus turpis orci, in consectetur nisi aliquet a. Duis vitae rhoncus eros.",
-    "Praesent enim sapien, consequat sed congue vitae, semper et arcu. Sed vel ultrices ex, eu fermentum mi.",
-    "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
+var description = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed justo dolor. Praesent convallis est ullamcorper risus tincidunt, eget viverra ante convallis. Duis rhoncus blandit accumsan.",
+  "Ut pretium molestie purus, vitae ornare tellus accumsan vel. Donec congue mi vitae rhoncus placerat. Nunc id tempus enim, at cursus tortor.",
+  "Fusce ipsum sem, fermentum sed quam eget, vestibulum fermentum lacus. In posuere tellus sit amet volutpat tempor. Sed feugiat lectus orci, quis mattis augue luctus eget.",
+  "Donec pretium nunc ex, in iaculis ex scelerisque in. Pellentesque a tellus vel arcu cursus commodo a iaculis arcu.",
+  "Nunc vel convallis ipsum. Mauris luctus turpis orci, in consectetur nisi aliquet a. Duis vitae rhoncus eros.",
+  "Praesent enim sapien, consequat sed congue vitae, semper et arcu. Sed vel ultrices ex, eu fermentum mi.",
+  "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit."
 ]
-
-$('#photo').attr('src', data.photo);
-$('#photo-title').text(data.title);
-$('#photo-description').text(data.description);
-
-$('#photo').attr('src', images[currentPhoto]);
-$('#photo-title').text(subtitles[currentPhoto]);
-$('#photo-description').text(explanations[currentPhoto]);
